7085718f2f05f7171cfc7702cc7bacfd7b01c5fc
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2015-07-29 20:34:13 +0300

    initial commit

---
+++ jquery-most.js
@@ -0,0 +1,17 @@
+/**
+ * MOST Web Framework
+ * A JavaScript Web Framework
+ * http://themost.io
+ *
+ * Copyright (c) 2015, Kyriakos Barbounakis k.barbounakis@gmail.com, Anthi Oikonomou anthioikonomou@gmail.com
+ *
+ * Released under the BSD 3-Clause license
+ * Date: 2015-07-29
+ *
+ * JQuery Extension for MOST Web Framework ver. 0.1.2
+ *
+ */
+(function() {
+    if (typeof window.jQuery === 'undefined') { return; }
+    if (typeof $ === 'undefined') { $ = window.jQuery; }
+})();
---
+++ most-client.js
@@ -0,0 +1,13 @@
+/**
+ * MOST Web Framework
+ * A JavaScript Web Framework
+ * http://themost.io
+ *
+ * Copyright (c) 2015, Kyriakos Barbounakis k.barbounakis@gmail.com, Anthi Oikonomou anthioikonomou@gmail.com
+ *
+ * Released under the BSD 3-Clause license
+ * Date: 2015-07-29
+ *
+ * MOST Web Framework Client ver. 0.1.2
+ *
+ */
